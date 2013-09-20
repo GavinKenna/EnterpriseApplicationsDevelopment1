2212db4662ca386aa14418a7cffb9affa196cbe9
Gavin Kenna <Gavin.Kenna@ittd.ie>
2013-09-20 14:55:15 +0100

    Lab3 Upload

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part 7
+            Sphere sphere = new Sphere(4.5);
+            Console.WriteLine(sphere.ToString());
+            Console.WriteLine("Sphere volume, called from IHasVolume,  is :: "+((IHasVolume)sphere).getVolume());
+
+            Console.WriteLine("\nPress any key to continue...\n");
+            Console.Read();
+
+            //Part 8
+            List<Sphere> spheres = new List<Sphere>();
+            Random r;
+            for (int i = 0; i < 10; i++)
+            {
+                r = new Random(i*i+20); //random seed
+                spheres.Add(new Sphere((double)r.Next(1,500)+r.NextDouble())); //random sphere radius
+            }
+
+            foreach (Sphere s in spheres)
+            {
+                Console.WriteLine(s.ToString());
+            }
+
+            Console.Read();
+            Console.Read();
+
+        }
+    }
+}
---
+++ Lab3/Lab3/Sphere.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Sphere:IHasVolume
+    {
+        private double radius;
+
+        public double Radius
+        {
+            get
+            {
+                return this.radius;
+            }
+            set
+            {
+                this.radius = value;
+            }
+        }
+
+        public Sphere(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double getVolume()
+        {
+            return Math.PI * radius * radius * radius;
+        }
+
+        public override string ToString()
+        {
+            return "Information about this sphere :: \nRadius is :: "+this.radius + " \nVolume is :: " + this.getVolume()+"\n";
+        }
+    }
+}
