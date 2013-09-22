e1fb80324df6d459128f6e975626df02de12a4ca
Gavin Kenna <Gavin.Kenna@ittd.ie>
2013-09-22 14:03:06 +0100

    Added Lab5

---
+++ Lab5/Lab5/Money.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{ 
+    enum CURRENCY{EURO, USDOLLARS, YEN};
+
+    struct Money
+    {
+        public double Amount { get; set; }
+        public CURRENCY Currency { get; set; }
+
+        const double OneEuroToYen = 134.3645;
+        const double OneEuroToUSD = 1.35269;
+
+        public Money(double amount, CURRENCY currency) : this()
+        {
+            this.Amount = amount;
+            this.Currency = currency;
+        }
+
+        public double Convert(CURRENCY toCurrency)
+        {
+            //First, convert currency to euro as we have the conversion rate for it
+            double convertedToEuro = this.Amount;
+            if (this.Currency != CURRENCY.EURO)
+            {
+                if (this.Currency == CURRENCY.USDOLLARS)
+                {
+                    convertedToEuro = Amount / OneEuroToUSD;
+                }
+                else if (this.Currency == CURRENCY.YEN)
+                {
+                    convertedToEuro = Amount / OneEuroToYen;
+                }
+            }
+
+            //Now, convert that to the chosen currency
+
+            if (toCurrency == CURRENCY.EURO)
+            {
+                return convertedToEuro;
+            }
+            else if (toCurrency == CURRENCY.USDOLLARS)
+            {
+                return convertedToEuro * OneEuroToUSD;
+            }
+            else if (toCurrency == CURRENCY.YEN)
+            {
+                return convertedToEuro * OneEuroToYen;
+            }
+
+            return 0.0;
+        }
+
+        public static Money operator +(Money a, Money b)
+        {
+            if (a.Currency == b.Currency)
+            {
+                return new Money(a.Amount + b.Amount, a.Currency);
+            }
+            else
+            {
+                return new Money(a.Amount + b.Convert(a.Currency), a.Currency);
+            }
+
+        }
+    }
+}
---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money m1 = new Money(14.50, CURRENCY.EURO);
+            Money m2 = new Money(29.13, CURRENCY.USDOLLARS);
+            Money m3 = new Money(2.33, CURRENCY.YEN);
+
+            Console.WriteLine("Euro :: " + m1.Amount + " to USD is :: " + m1.Convert(CURRENCY.USDOLLARS));
+            Console.WriteLine("Euro :: " + m1.Amount + " to YEN is :: " + m1.Convert(CURRENCY.YEN));
+            Console.WriteLine();
+
+            Console.WriteLine("Yen :: " + m3.Amount + " to USD is :: " + m3.Convert(CURRENCY.USDOLLARS));
+            Console.WriteLine("Yen :: " + m3.Amount + " to EURO is :: " + m3.Convert(CURRENCY.EURO));
+            Console.WriteLine();
+
+            Console.WriteLine("USD :: " + m2.Amount + " to EURO is :: " + m2.Convert(CURRENCY.EURO));
+            Console.WriteLine("USD :: " + m2.Amount + " to YEN is :: " + m2.Convert(CURRENCY.YEN));
+            Console.WriteLine();
+
+            //Adding two Money objects together
+
+            Money a = new Money(13.99, CURRENCY.EURO);
+            Money b = new Money(99.34, CURRENCY.USDOLLARS);
+
+            Console.WriteLine("Euro :: " + a.Amount + " + USD :: " + b.Amount + " = Euro :: " + (a + b).Amount);
+
+            Console.Read();
+        }
+    }
+}
